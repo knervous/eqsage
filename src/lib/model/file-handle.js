b492cb7ecf9b8cac3783f0b48f370c3982798687
Paul Johnson <Paul@PJOHNSOMAC-6366.digi.box>
2024-02-23 22:27:22 -0500

    no khr extension
    Former-commit-id: cfc209c337d9ce5743ef7c593b148348db7780fc

--- src/lib/model/file-handle.js
+++ src/lib/model/file-handle.js
@@ -4,7 +4,7 @@ import { Document } from '@gltf-transform/core';
 import { EQGDecoder } from '../eqg/eqg-decoder';
 import { getEQFile } from '../util/fileHandler';
 
-export const VERSION = 1.0;
+export const VERSION = 1.1;
 
 export class EQFileHandle {
   /**
