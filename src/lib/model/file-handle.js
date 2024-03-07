cff16f46b418cf4c1c2b3db827fdcf0f3d572e2c
Paul Johnson <Paul@PJOHNSOMAC-6366.digi.box>
2024-03-07 06:31:17 -0500

    bump version

--- src/lib/model/file-handle.js
+++ src/lib/model/file-handle.js
@@ -4,7 +4,7 @@ import { Document } from '@gltf-transform/core';
 import { EQGDecoder } from '../eqg/eqg-decoder';
 import { getEQFile } from '../util/fileHandler';
 
-export const VERSION = 1.1;
+export const VERSION = 1.2;
 
 export class EQFileHandle {
   /**
