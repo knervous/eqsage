6075c406d16a8ec37d52842265a8bb24c60935b6
Paul Johnson <pjohnson@minitab.de>
2025-02-09 00:40:51 -0500

    add everything for animations
    Former-commit-id: fe189a4840511424b3d02e8b7505d66622419984

---
+++ src/components/common/nav/nav-footer.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Box, Stack } from '@mui/material';
+import './nav.scss';
+
+export const NavFooter = ({ children, offset = false, minWidth = '400px', height = '80px' }) => {
+  const bgOptions = {
+    minWidth: `calc(${minWidth} + 4px)`,
+    height  : `calc(${height} + 2px)`,
+  };
+  const options = { minWidth, height };
+  if (offset) {
+    bgOptions.left = 'calc(25vw - 252px) !important';
+    options.left = 'calc(25vw - 250px) !important';
+  }
+  return (
+    <>
+      <Box className="builder-footer-bg" sx={bgOptions} />
+      <Box className="builder-footer" sx={options}>
+        <Box
+          sx={{
+            padding: '12px 8vw',
+          }}
+        >
+          <Stack
+            direction="row"
+            justifyContent={'center'}
+            alignContent={'center'}
+            alignItems={'center'}
+          >
+            {children}
+          </Stack>
+        </Box>
+      </Box>
+    </>
+  );
+};
