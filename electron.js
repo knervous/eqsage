f66fb413603a6e72c882c94588002c8957058462
Paul Johnson <pcj174@gmail.com>
2025-03-07 03:46:45 -0800

    offer opt out of auto update

--- electron.js
+++ electron.js
@@ -6,14 +6,165 @@ import {
   dialog,
   nativeImage,
   screen,
+  Menu,
 } from "electron";
 import pkg from "electron-updater";
 import path from "path";
 import { fsInterface } from "./src/fsInterface.js";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import Store from "electron-store";
 
 const { autoUpdater } = pkg;
+const isMac = process.platform === "darwin";
+const store = new Store();
+function createMenu() {
+  const menuTemplate = [
+    // { role: 'appMenu' }
+    ...(isMac
+      ? [
+          {
+            label: app.name,
+            submenu: [
+              { role: "about" },
+              { type: "separator" },
+              { role: "services" },
+              { type: "separator" },
+              { role: "hide" },
+              { role: "hideOthers" },
+              { role: "unhide" },
+              { type: "separator" },
+              { role: "quit" },
+            ],
+          },
+        ]
+      : []),
+    // { role: 'fileMenu' }
+    {
+      label: "File",
+      submenu: [isMac ? { role: "close" } : { role: "quit" }],
+    },
+    {
+      label: 'Auto-Update',
+      submenu: [
+        {
+          label: `Toggle Auto-Update`,
+          click: () => {
+            const current = store.get('autoUpdateEnabled');
+            // Toggle the setting.
+            store.set('autoUpdateEnabled', !current);
+            const message = !current ? 'Auto update enabled.' : 'Auto update disabled.';
+            dialog.showMessageBox({ message });
+          }
+        },
+      ]
+    },
+    // { role: 'editMenu' }
+    {
+      label: "Edit",
+      submenu: [
+        { role: "undo" },
+        { role: "redo" },
+        { type: "separator" },
+        { role: "cut" },
+        { role: "copy" },
+        { role: "paste" },
+        ...(isMac
+          ? [
+              { role: "pasteAndMatchStyle" },
+              { role: "delete" },
+              { role: "selectAll" },
+              { type: "separator" },
+              {
+                label: "Speech",
+                submenu: [{ role: "startSpeaking" }, { role: "stopSpeaking" }],
+              },
+            ]
+          : [{ role: "delete" }, { type: "separator" }, { role: "selectAll" }]),
+      ],
+    },
+    // { role: 'viewMenu' }
+    {
+      label: "View",
+      submenu: [
+        { role: "reload" },
+        { role: "forceReload" },
+        { role: "toggleDevTools" },
+        { type: "separator" },
+        { role: "resetZoom" },
+        { role: "zoomIn" },
+        { role: "zoomOut" },
+        { type: "separator" },
+        { role: "togglefullscreen" },
+      ],
+    },
+    // { role: 'windowMenu' }
+    {
+      label: "Window",
+      submenu: [
+        { role: "minimize" },
+        { role: "zoom" },
+        ...(isMac
+          ? [
+              { type: "separator" },
+              { role: "front" },
+              { type: "separator" },
+              { role: "window" },
+            ]
+          : [{ role: "close" }]),
+      ],
+    },
+    {
+      role: "help",
+      submenu: [
+        {
+          label: "Learn More",
+          click: async () => {
+            const { shell } = require("electron");
+            await shell.openExternal("https://electronjs.org");
+          },
+        },
+      ],
+    },
+  ];
+
+  const menu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(menu);
+}
+
+// Set a default for auto updates if it hasn't been set already.
+if (store.get("autoUpdateEnabled") === undefined) {
+  store.set("autoUpdateEnabled", true);
+}
+
+function shouldCheckForUpdates() {
+  return store.get("autoUpdateEnabled");
+}
+
+autoUpdater.on('update-available', e => {
+  console.log('Update', e);
+  const response = dialog.showMessageBoxSync({
+    type: 'info',
+    buttons: ['Download', 'Cancel'],
+    title: 'Update Available',
+    message: `A new EQ Sage version ${e.version} is available. Do you want to download it?`
+  });
+  if (response === 0) { // User clicked 'Download'
+    autoUpdater.downloadUpdate();
+  }
+});
+
+autoUpdater.on('update-downloaded', () => {
+  const response = dialog.showMessageBoxSync({
+    type: 'info',
+    buttons: ['Install and Restart', 'Later'],
+    title: 'Update Ready',
+    message: 'Update downloaded. Would you like to install it now?'
+  });
+  if (response === 0) {
+    autoUpdater.quitAndInstall();
+  }
+});
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
@@ -43,7 +194,7 @@ function createWindow() {
   if (process.env.LOCAL_DEV === "true") {
     console.log("Loading local dev");
     // win.loadURL("http://localhost:4200");
-    win.loadFile(path.join(__dirname, 'build', 'index.html'));
+    win.loadFile(path.join(__dirname, "build", "index.html"));
   } else {
     win.loadFile(path.join(__dirname, "build", "index.html"));
   }
@@ -61,13 +212,22 @@ app.whenReady().then(() => {
   }
 
   createWindow();
+  createMenu();
+
+  // Prevent auto-download; control downloads manually.
+  autoUpdater.autoDownload = false;
+
+  // Check for updates only if the user has enabled auto updates.
+  if (shouldCheckForUpdates()) {
+    autoUpdater.checkForUpdates();
+  } else {
+    console.log('User did not want to check for auto updates.');
+  }
 });
 
-// if (process.env.LOCAL_DEV === 'true') {
-//   autoUpdater.forceDevUpdateConfig = true;
-// }
-
-autoUpdater.checkForUpdatesAndNotify();
+if (process.env.LOCAL_DEV === 'true') {
+  // autoUpdater.forceDevUpdateConfig = true;
+}
 
 // Quit the app when all windows are closed (except on macOS).
 app.on("window-all-closed", () => {
